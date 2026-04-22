2e649d637be06153f9213dfb1312177473420106
dujun <dujun_dujun@126.com>
2026-04-22 18:09:17 +0800

    Fix: default language to English, remove browser navigator detection

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -35,7 +35,7 @@ i18n
             escapeValue: false,
         },
         detection: {
-            order: ['localStorage', 'navigator'],
+            order: ['localStorage'],
             caches: ['localStorage'],
         },
     });
